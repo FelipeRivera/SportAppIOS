d6970204e67a1833266eeea774f83c09962cafc0
Felipe Rivera <yf.rivera1851@uniandes.edu.co>
2025-03-10 19:24:48 -0300

    Initial Commit

---
+++ sportApp/sportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportAppApp.swift
+//  sportApp
+//
+//  Created by Felipe Rivera on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct sportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
